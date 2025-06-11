37c4a3a26766aabb0cf21088ac742d2ec00de2fa
Ankit Tiwari <ankit827691@gmail.com>
2025-06-11 16:45:43 +0530

    Add answer model schema with fields for author, content, and metadata

---
+++ src/models/core/answer.model.js
@@ -0,0 +1,81 @@
+import mongoose, {Schema} from "mongoose"
+
+const answerSchema = new Schema(
+    {
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        authorName: {
+            type: String,
+        },
+        authorAvatar: {
+            type: String
+        },
+        question: {
+            type: Schema.Types.ObjectId,
+            ref: "Question"
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        images: {
+            type: [String],
+            default: []
+        },
+        isAccepted: {
+            type: Boolean,
+            default: false
+        },
+        likesCount: {
+            type: Number,
+            default: 0
+        },
+        dislikesCount: {
+            type: Number,
+            default: 0
+        },
+        bookmarkedBy: [{
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }],
+        commentsCount: {
+            type: Number,
+            default: 0
+        },
+        isFlagged: {
+            type: Boolean,
+            default: false
+        },
+        flagReason: {
+            type: String
+        },
+        isReviewed: {
+            type: Boolean,
+            default: false
+        },
+        report: {
+            type: [
+                {
+                    reportid: {
+                        type: Schema.Types.ObjectId,
+                        ref: "User",
+                        required: true
+                    },
+                    reportText: String 
+                }
+            ],
+            default: []
+        },
+        isEdited: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+export const Answer = mongoose.model("Answer", answerSchema)
