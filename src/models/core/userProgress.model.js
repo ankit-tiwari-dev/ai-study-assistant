f8511212fa1dbc153397a39342ce9a76c3dad94d
Ankit Tiwari <ankit827691@gmail.com>
2025-06-12 15:14:09 +0530

    Add user progress model schema with fields for user, subject, topic, completion percentage, last access time, total time spent, sessions count, milestones, quizzes taken, average score, last score, last quiz taken time, status, completion status, certification status, and notes

---
+++ src/models/core/userProgress.model.js
@@ -0,0 +1,85 @@
+import mongoose, { Schema } from "mongoose";
+
+const milestoneSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completedAt: {
+        type: Date
+    }
+})
+
+const userProgressSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        subject: {
+            type: Schema.Types.ObjectId,
+            ref: "Subject"
+        },
+        topic: {
+            type: Schema.Types.ObjectId,
+            ref: "Topic"
+        },
+        completionPercentage: {
+            type: Number,
+            default: 0
+        },
+        lastAccessAt: {
+            type: Date
+        },
+        totalTimeSpent: {
+            type: Number,
+            default: 0
+        },
+        sessionsCount: {
+            type: Number,
+            default: 0
+        },
+        milestones: [milestoneSchema],
+        quizzesTaken: {
+            type: Number,
+            default: 0
+        },
+        averageScore: {
+            type: Number,
+            default: 0
+        },
+        lastScore: {
+            type: Number,
+            default: 0
+        },
+        lastQuizTakenAt: {
+            type: Date,
+        },
+        status: {
+            type: String,
+            enum: ["in_progress", "completed", "not_started"],
+            default: "not_started"
+        },
+        isCompleted: {
+            type: Boolean,
+            default: false
+        },
+        certified: {
+            type: Boolean,
+            default: false
+        },
+        notes: {
+            type: [String],
+            default: []
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const UserProgress = mongoose.model("UserProgress", userProgressSchema)
