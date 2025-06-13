e2633a102342e0427ec66ca20a8e5c764aeaf4b2
Ankit Tiwari <ankit827691@gmail.com>
2025-06-13 22:40:59 +0530

    Add core model imports: include Question, Answer, Article, Follow, Comment, Feedback, Notification, Bookmark, Report, Subject, Topic, User, and UserProgress

---
+++ src/models/core/index.js
@@ -0,0 +1,30 @@
+import { Question } from "./question.model.js"
+import { Answer } from "./answer.model.js"
+import { Article } from "./article.model.js"
+import { Follow } from "./follow.model.js"
+import { Comment } from "./comment.model.js"
+import { Feedback } from "./feedback.model.js"
+import { Notification } from "./notification.model.js"
+import { Bookmark } from "./bookmark.model.js" 
+import { Report } from "./report.model.js"
+import { Subject } from "./subject.model.js"
+import { Topic } from "./topic.model.js"
+import { User } from  "./user.model.js"
+import { UserProgress } from  "./userProgress.model.js"
+
+
+export {
+    Question,
+    Answer,
+    Article,
+    Follow,
+    Comment,
+    Feedback,
+    Notification,
+    Bookmark,
+    Report,
+    Subject,
+    Topic,
+    User,
+    UserProgress
+}
