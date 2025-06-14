8f90509874954c47badca091718d7723a5eb1768
Ankit Tiwari <ankit827691@gmail.com>
2025-06-14 23:11:39 +0530

    Fix import statements and update exports in ai model index file

--- src/models/ai/index.js
+++ src/models/ai/index.js
@@ -1,13 +1,21 @@
-import { AiFeedback } from "./feedback.model";
-import { AiLog } from "./aiLog.model"
-import { AiRecommendation } from "./recommendation.model"
-import { SearchHistory } from "./searchHistory.model"
-import { UsageLimit } from "./usageLimit.model"
+import { AiFeedback } from "./aiFeedback.model.js";
+import { AiLog } from "./aiLog.model.js"
+import { AiRecommendation } from "./recommendation.model.js"
+import { SearchHistory } from "./searchHistory.model.js"
+import { UsageLimit } from "./usageLimit.model.js"
+import { AiVideoExplain } from "./aiVideoExplain.model.js"
+import { AiIssueReport } from "./aiIssueReport.model.js"
+import { AiLearningProfile } from "./aiLearningProfile.model.js"
+import { AiPerformance } from "./aiPerformance.model.js"
 
 export {
     AiFeedback,
     AiLog,
     AiRecommendation,
     SearchHistory,
-    UsageLimit
+    UsageLimit,
+    AiVideoExplain,
+    AiIssueReport,
+    AiLearningProfile,
+    AiPerformance
 }
