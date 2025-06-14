dfd79b9c1e8e798e47e8df4e0a1673ac51bf75d3
Ankit Tiwari <ankit827691@gmail.com>
2025-06-14 22:57:13 +0530

    Add aiPerformance schema: define structure for AI performance tracking

---
+++ src/models/ai/aiPerformance.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+
+const AiPerformanceSchema =  new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        iqScore: {
+            type: Number,
+            default: 0
+        },
+        badge: {
+            type: String,
+            enum: ["Beginner", "Intermediate", "Advanced", "Master", "GrandMaster", "Genius", "Expert"]
+        },
+        testType: {
+            type: String,
+            enum: ["IQ Test", "Subject Quiz"]
+        },
+        subject: {
+            type: Schema.Types.ObjectId,
+            ref: "Subject"
+        },
+        topic: {
+            type: Schema.Types.ObjectId,
+            ref: "Topic"
+        },
+        score: {
+            type: Number,
+            min: 0,
+            default: 0
+        },
+        timeTaken: {
+            type: Number
+        },
+        level:{
+            type: String,
+            enum: ["Beginner", "Intermediate", "Advanced", "Master", "GrandMaster", "Genius", "Expert"]
+        },
+        aiSummary: {
+            type: String,
+            default: 0
+        }
+    },
+    {
+        timestamps: true
+    }
+)
