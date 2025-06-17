4aa37af83573b2149ac17cdd00ef3bb103361cad
Ankit Tiwari <ankit827691@gmail.com>
2025-06-17 11:50:14 +0530

    Add monetization models: import Course, Credit, Invoice, Payment, PaymentLog, Subscription, Transaction, UsageLog, UserCredit, UserPurchase, and UserSubscription

---
+++ src/models/monetization/index.js
@@ -0,0 +1,25 @@
+import { Course } from "./course.model";
+import { Credit } from "./credit.model";
+import { Invoice } from "./invoice.model";
+import { Payment } from "./payment.model";
+import { PaymentLog } from "./paymentLog.model";
+import { Subscription } from "./subscription.model";
+import { Transaction } from "./transaction.model";
+import { UsageLog } from "./usageLog.model";
+import { UserCredit } from "./userCredit.model";
+import { UserPurchase } from "./userPurchase.model";
+import { UserSubscription } from "./userSubscription.model";
+
+export {
+    Course,
+    Credit,
+    Invoice,
+    Payment,
+    PaymentLog,
+    Subscription,
+    Transaction,
+    UsageLog,
+    UserCredit,
+    UserPurchase,
+    UserSubscription
+}
